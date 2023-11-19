1f38a61a14bc60f4ffb2dccba401be1df1cfb627
marble <git@computer-in.love>
2023-11-19 23:17:53 +0100

    remove dead code

--- amaranth_companion/module/scene.py
+++ amaranth_companion/module/scene.py
@@ -58,30 +58,3 @@ class ModuleScene(QGraphicsScene):
         if lines_major:
             painter.setPen(self._pen_major)
             painter.drawLines(*lines_major)
-
-    # def addDebugContent(self):
-    #     greenBrush = QBrush(Qt.green)
-    #     outlinePen = QPen(Qt.black)
-    #     outlinePen.setWidth(2)
-    #
-    #     rect = self.addRect(100, 100, 80, 100, outlinePen, greenBrush)
-    #     rect.setFlag(QGraphicsItem.ItemIsMovable)
-    #
-    #     text = self.addText("This is my Awesome text!", QFont("Ubuntu"))
-    #     text.setFlag(QGraphicsItem.ItemIsSelectable)
-    #     text.setFlag(QGraphicsItem.ItemIsMovable)
-    #     text.setDefaultTextColor(QColor.fromRgbF(1.0, 1.0, 1.0))
-    #
-    #     widget1 = QPushButton("Hello World")
-    #     proxy1 = self.addWidget(widget1)
-    #     proxy1.setFlag(QGraphicsItem.ItemIsMovable)
-    #     proxy1.setPos(200, 30)
-    #
-    #     widget2 = QTextEdit()
-    #     proxy2 = self.addWidget(widget2)
-    #     proxy2.setFlag(QGraphicsItem.ItemIsSelectable)
-    #     proxy2.setPos(200, 60)
-    #
-    #     line = self.addLine(300, 300, 400, 500, outlinePen)
-    #     line.setFlag(QGraphicsItem.ItemIsMovable)
-    #     line.setFlag(QGraphicsItem.ItemIsSelectable)
